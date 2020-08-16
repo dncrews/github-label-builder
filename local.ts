d69799acf9d931322b8a6b32728d99c82c02be2a
Dan Crews <crewsd@gmail.com>
2020-08-15 19:59:35 -0700

    refactor(svg): nest under 'src'

--- local.ts
+++
@@ -1,3 +0,0 @@
-import { app } from './server'
-
-app.listen(3000)
---
+++ src/local.ts
@@ -0,0 +1,8 @@
+import { AddressInfo } from 'net';
+import { app } from './server'
+
+const server = app.listen(3000, () => {
+  const { port } = server.address() as AddressInfo;
+  console.info({ address: server.address() })
+  console.info(`http://localhost:${port}/passing/listening.svg`);
+})
