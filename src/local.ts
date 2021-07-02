c8f6282ade5fdfcb34076149093d8035087b1b54
Dan Crews <crewsd@gmail.com>
2021-07-02 16:12:18 -0600

    chore: remove extraneous console.log

--- src/local.ts
+++ src/local.ts
@@ -3,6 +3,5 @@ import { app } from './server'
 
 const server = app.listen(3000, () => {
   const { port } = server.address() as AddressInfo;
-  console.info({ address: server.address() })
   console.info(`http://localhost:${port}/passing/listening.svg`);
 })
